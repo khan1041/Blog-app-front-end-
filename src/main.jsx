f9c7e1f733e70954a8575ab1eca1d97ccede3937
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-01-10 20:12:23 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider1 } from './Context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
