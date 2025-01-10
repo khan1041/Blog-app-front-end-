92146cd0a0cec2014ea01a4d452410352f20c149
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-01-10 17:39:31 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,16 @@
 import App from './App.jsx'
 import './index.css'
 
-import { Provider } from 'react-redux'
-//import { store } from './app/store.js'
-//import { ToastContainer} from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify'
 import { AuthProvider1 } from './Context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
    <StrictMode>
-  {/* <Provider store={store}>  */}
  <AuthProvider1> 
 <App />
 
  </AuthProvider1> 
- {/* </Provider> */}
 <ToastContainer/>
   </StrictMode>
   </BrowserRouter>
